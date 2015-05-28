97a379836d2f5cb868d629cca193365bc471cb8f
MAlex <alexmaximovj@gmail.com>
2015-05-28 20:03:29 +0300

    add

--- src/main/java/game/SnakeGame.java
+++ src/main/java/game/SnakeGame.java
@@ -1,18 +1,31 @@
 package game;
 
+import entity.Snake;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
-public class SnakeGame extends JPanel {
+public class SnakeGame extends JPanel implements ActionListener {
 
     public static final int SCALE = 32;
     public static final int WIDTH = 20;
     public static final int HEIGTH = 20;
+    public static final int SPEED = 5;
+
+    public Snake snake = new Snake(10, 10, 9, 10);
+    Timer timer = new Timer(1000 / SPEED, this);
 
     public SnakeGame() {
-
+        timer.start();
+        addKeyListener(new KeyBoard());
+        setFocusable(true);
     }
 
+    @Override
     public void paint(Graphics graph) {
         graph.setColor(color(20, 20, 50));
         graph.fillRect(0, 0, WIDTH * SCALE, HEIGTH * SCALE);
@@ -26,6 +39,11 @@ public class SnakeGame extends JPanel {
             graph.drawLine(0, axisY, WIDTH * SCALE, axisY);
         }
 
+        for (int d = 0; d < snake.length; d++) {
+
+            graph.setColor(color(200, 200, 10));
+            graph.fillRect(snake.snakeX[d] * SCALE + 1, snake.snakeY[d] * SCALE + 1, SCALE - 1, SCALE - 1);
+        }
 
     }
 
@@ -46,7 +64,7 @@ public class SnakeGame extends JPanel {
         frame.setResizable(false);
 
         // The method changes the geometry-related data.
-        frame.setSize(WIDTH * SCALE+1, HEIGTH * SCALE+1);
+        frame.setSize(WIDTH * SCALE + 1, HEIGTH * SCALE + 1);
 
         // Add JPanel (SnakeGame) to JFrame
         frame.add(new SnakeGame());
@@ -57,6 +75,36 @@ public class SnakeGame extends JPanel {
 
         //Shows or hides this {@code Window} depending on the value of parameter
         frame.setVisible(true);
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        snake.move();
+
+        repaint();
+    }
+
+    //Add KeyAdapter
+    private class KeyBoard extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent keyEvent) {
+
+            int key = keyEvent.getKeyCode();
+
+            if ((key == KeyEvent.VK_RIGHT) & snake.direction != 2) {
+                snake.direction = 0;
+            }
+            if ((key == KeyEvent.VK_DOWN) & snake.direction != 3) {
+                snake.direction = 1;
+            }
+            if ((key == KeyEvent.VK_LEFT) & snake.direction != 0) {
+                snake.direction = 2;
+            }
+            if ((key == KeyEvent.VK_UP) & snake.direction != 1) {
+                snake.direction = 3;
+            }
+        }
 
     }
 }
